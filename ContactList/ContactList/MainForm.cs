a3f91c594420d313e2ddccc0823c176e9f538750
VoThaiToan <vothaitoan28042004@gmail.com>
2025-05-28 10:08:50 +0700

    Them Form de them lien he moi

--- ContactList/ContactList/MainForm.cs
+++ ContactList/ContactList/MainForm.cs
@@ -6,5 +6,11 @@ namespace ContactList
         {
             InitializeComponent();
         }
+
+        private void btn_Add_Click(object sender, EventArgs e)
+        {
+            AddContactForm addForm = new AddContactForm();
+            addForm.ShowDialog();
+        }
     }
 }
